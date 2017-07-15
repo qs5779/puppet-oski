f4ceed99a471e08d3bcae4f505684ae8c2bdf5cd
Quien Sabe <quiensabe@wtfo-guru.com>
2017-07-15 14:14:49 -0400

    check for file existence, don't simply assume

--- lib/facter/sshfp.rb
+++ lib/facter/sshfp.rb
@@ -2,24 +2,34 @@ Facter.add("ssh_dsa_fp") do
     confine :kernel => "Linux"
     fqdn=Facter.value("fqdn")
     setcode do
-        fp = %x[ssh-keygen -f /etc/ssh/ssh_host_dsa_key.pub -r #{fqdn} | cut -d ' ' -f 6]
-        if not fp 
-            "unknown"
+        # good practice is to allways test for a file's existence
+        if File.exists? '/etc/ssh/ssh_host_dsa_key.pub'
+            fp = %x[ssh-keygen -f /etc/ssh/ssh_host_dsa_key.pub -r #{fqdn} | cut -d ' ' -f 6]
+            if not fp
+                "unknown"
+            else
+                fp
+            end
         else
-            fp
-        end 
+            "unknown"
+        end
     end
 end
 
 Facter.add(:ssh_rsa_fp) do
     confine :kernel => "Linux"
     setcode do
-        fqdn=Facter.value("fqdn")
-        fp = %x[ssh-keygen -f /etc/ssh/ssh_host_rsa_key.pub -r #{fqdn} | cut -d ' ' -f 6]
-        if not fp
-            "unknown"
+        # good practice is to allways test for a file's existence
+        if File.exists? '/etc/ssh/ssh_host_rsa_key.pub'
+            fqdn=Facter.value("fqdn")
+            fp = %x[ssh-keygen -f /etc/ssh/ssh_host_rsa_key.pub -r #{fqdn} | cut -d ' ' -f 6]
+            if not fp
+                "unknown"
+            else
+                fp
+            end
         else
-            fp
+            "unknown"
         end
     end
 end
